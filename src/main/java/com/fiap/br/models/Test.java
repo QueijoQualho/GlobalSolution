3fe21046cc2f0c34e5575bee36485647436e81cf
Pedro Jesus <pedromra.04@gmail.com>
2024-05-22 17:38:18 -0300

    Updated .gitignore, pom.xml, and Test.java files with new dependencies, annotations, and properties.

--- src/main/java/com/fiap/br/models/Test.java
+++ src/main/java/com/fiap/br/models/Test.java
@@ -1,6 +1,27 @@
 package com.fiap.br.models;
 
+import com.fiap.br.util.annotations.CollumnName;
+import com.fiap.br.util.annotations.TableName;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@TableName("teste")
 public class Test {
-    
+    @CollumnName("id")
+    int id;
+
+    @CollumnName("nome")
+    @NotNull(message = "O nome é obrigatório")
+    @Size(min = 2, max = 50, message = "O nome deve ter entre 2 e 50 caracteres")
+    String nome;
+
+    @CollumnName("valor")
+    @NotNull(message = "O Valor é obrigatório")
+    Double valor;
+
+    @CollumnName("descricao")
+    @NotNull(message = "O descricao é obrigatório")
+    @Size(min = 2, max = 255, message = "O nome deve ter entre 2 e 255 caracteres")
+    String descricao;
 }
---
+++ src/main/java/com/fiap/br/util/providers/ConstraintValidator.java
@@ -0,0 +1,27 @@
+package com.fiap.br.util.providers;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Provider
+public class ConstraintValidator implements ExceptionMapper<ConstraintViolationException> {
+
+    @Override
+    public Response toResponse(ConstraintViolationException exception) {
+        Map<String, String> errors = new HashMap<>();
+        Set<ConstraintViolation<?>> violations = exception.getConstraintViolations();
+        for (ConstraintViolation<?> violation : violations) {
+            String propertyPath = violation.getPropertyPath().toString();
+            String message = violation.getMessage();
+            errors.put(propertyPath, message);
+        }
+
+        return Response.status(Response.Status.BAD_REQUEST).entity(errors).build();
+    }
+}
